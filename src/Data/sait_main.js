1cc0e74bcc27d054ec58deb782343380668d7ae2
SanviBongale28 <bongalesanvi@gmail.com>
2024-09-15 16:46:54 +0530

    main-board info updated

--- src/Data/sait_main.js
+++ src/Data/sait_main.js
@@ -1,221 +1,292 @@
 const data = [
   
   {
-    Name: "Aaditya Khot",
+    Name: "Parag Patkulkar",
     Board: "Main",
     Position: "President",
     Photo:
-      "https://res.cloudinary.com/du3cgjere/image/upload/v1693624822/sait%20images/Aaditya_Khot_mdefbn.jpg",
-    LinkedInProfile:
-      "https://www.linkedin.com/in/aaditya-khot-111672229?fbclid=PAAaZDIuIFS2QRQ6CoGWm86_SwCeAOELYfAjNmcVcw69YzWOoIcu68smliHEg&original_referer=https%3A%2F%2Fl.instagram.com%2F",
-    GitHubProfile: "https://github.com/aadityafour7",
+      "https://res.cloudinary.com/du3cgjere/image/upload/v1693624851/sait%20images/Parag_Patkulkar_tynhxe.jpg",
+    LinkedInProfile: "https://linkedin.com/in/parag-patkulkar-ba00891a8",
+    GitHubProfile: "https://github.com/Quanta3",
     InstagramProfile:
-      "https://instagram.com/aaditya._.47?igshid=MzRlODBiNWFlZA==",
+        "https://www.instagram.com/_para.g403/",
   },
 
   {
-    Name: "Vedant Vedpathak",
+    Name: "Prapti Gawande ",
     Board: "Main",
     Position: "Vice President",
     Photo:
-      "https://res.cloudinary.com/du3cgjere/image/upload/v1693624878/sait%20images/Vedant_Vedpathak_llp6e0.jpg",
-    LinkedInProfile: "https://www.linkedin.com/in/vedant-vedpathak-b54384229",
-    GitHubProfile: "https://github.com/vedantv3",
-    InstagramProfile:
-      "https://instagram.com/_vedantv3_?utm_source=qr&igshid=MzNlNGNkZWQ4Mg%3D%3D",
+      "https://res.cloudinary.com/du3cgjere/image/upload/v1693624854/sait%20images/Prapti_Gawande_saci3u.jpg",
+    LinkedInProfile: "https://www.linkedin.com/in/prapti-gawande-54003b25a",
+    GitHubProfile: "https://github.com/praptigawande2812",
+    InstagramProfile: 
+        "https://www.instagram.com/praptigawande_28?igsh=cnJ6azZoZ29zZmli",
   },
-  
+
   {
-    Name: "Pranav Kadam",
+    Name: "Prathamesh Patil",
     Board: "Main",
     Position: "Secretary",
     Photo:
-      "https://res.cloudinary.com/du3cgjere/image/upload/v1693624852/sait%20images/Pranav_Kadam_jujkis.jpg",
-    LinkedInProfile: "https://www.linkedin.com/in/pranav-kadam-7947b5232/",
-    GitHubProfile: "github.com/pranav-kadam",
-    InstagramProfile: "-",
+      "https://res.cloudinary.com/dk9fzvliu/image/upload/v1726412377/PrathameshPatilNew_wqm0n1.jpg",
+    LinkedInProfile:"https://www.linkedin.com/in/prathamesh-patil-20b8aa28b",
+    GitHubProfile: "https://github.com/94prathampatil",
+    InstagramProfile: 
+        "https://www.instagram.com/07_pratham_patil",
   },
+
   {
-    Name: "Mitali Patil ",
+    Name: "Vedant Lonkar",
     Board: "Main",
     Position: "Secretary ",
     Photo:
-      "https://res.cloudinary.com/du3cgjere/image/upload/v1693624850/sait%20images/Mitali_Patil_n7cc6z.jpg",
-    LinkedInProfile: "https://www.linkedin.com/in/mitali-patil-3ab106231",
-    GitHubProfile: "https://github.com/mitupatil18",
+      "https://res.cloudinary.com/dk9fzvliu/image/upload/v1726411090/VedantLonkar_z6a4sq.jpg",
+    LinkedInProfile: "https://www.linkedin.com/in/vedant-lonkar-4a37a91b8?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
+    GitHubProfile: "https://github.com/vedantlonkar23",
     InstagramProfile:
-      "https://instagram.com/mitaliiiiipatil?igshid=MzRlODBiNWFlZA==",
+      "https://www.instagram.com/mevedantlonkar?igsh=ZTdwZ29jOTRnZ3N6",
   },
-  
+
   {
-    Name: "Viraj Takone",
+    Name: "Atharva Khot",
     Board: "Main",
     Position: "Program Director",
     Photo:
-      "https://res.cloudinary.com/du3cgjere/image/upload/v1693624880/sait%20images/Viraj_Takone_noff14.jpg",
-    LinkedInProfile: "https://www.linkedin.com/in/viraj-takone-a93745224",
-    GitHubProfile: "https://GitHub.com/viru04",
-    InstagramProfile: "https://instagram.com/viraj_a_t",
+      "https://res.cloudinary.com/dk9fzvliu/image/upload/v1726410173/Atharva_-_Kavita_Khot_b8akqj.jpg",
+    LinkedInProfile: "https://www.linkedin.com/in/atharva-khot-3b2479257?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
+    GitHubProfile: "https://github.com/atharvakhot78",
+    InstagramProfile: 
+        "https://www.instagram.com/_atharva017?igsh=MWJoMmZzMWQwMGoyYQ==",
   },
-  
+
   {
-    Name: "Shriya Patil ",
+    Name: "Mahemud Borgave",
     Board: "Main",
     Position: "Program Director ",
     Photo:
-      "https://res.cloudinary.com/du3cgjere/image/upload/v1693624873/sait%20images/Shriya_Patil_zsd80j.jpg",
-    LinkedInProfile: "https://www.linkedin.com/in/shriya-patil-b9082b234",
-    GitHubProfile: "https://github.com/shriya4545",
+      "https://res.cloudinary.com/dptdrzycj/image/upload/v1704356686/md_1_-_23620005_BORGAVE_MAHEMUD_USMAN_nnhxht.jpg",
+    LinkedInProfile: "https://www.linkedin.com/in/mahemud-borgave-b01762231",
+    GitHubProfile: "https://github.com/mahemudborgave",
     InstagramProfile:
-      "https://instagram.com/shriyapatil_?utm_source=qr&igshid=NGExMmI2YTkyZg%3D%3D",
+      "https://www.instagram.com/95_mahemud",
   },
-  
+
   {
-    Name: "Aaditya Patil ",
+    Name: "Vaibhav Kondekar",
     Board: "Main",
     Position: "Program Director ",
     Photo:
-      "https://res.cloudinary.com/du3cgjere/image/upload/v1693624821/sait%20images/Aaditya_Patil_krvs6w.jpg",
-    LinkedInProfile: "https://www.linkedin.com/in/aadi-exe",
-    GitHubProfile: "https://github.com/Aaditya123-exe",
+      "https://res.cloudinary.com/dk9fzvliu/image/upload/v1726410773/VaibhavKondekar_dsiczi.webp",
+    LinkedInProfile: "https://www.linkedin.com/in/vaibhav-kondekar-671234265?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
+    GitHubProfile: "https://github.com/vaibhavKondekar",
     InstagramProfile:
-      "https://instagram.com/aadityapatil345?utm_source=qr&igshid=NGExMmI2YTkyZg%3D%3D",
+      "https://www.instagram.com/_vaibhav_7897?igsh=MWdiZnJzeHpudXlvbg==",
   },
+
   {
-    Name: "Kedarnath Chavan",
+    Name: "Purva Patil",
     Board: "Main",
     Position: "Club Service Director",
     Photo:
-      "https://res.cloudinary.com/du3cgjere/image/upload/v1693624847/sait%20images/Kedarnath_Chavan_vc5qvt.jpg",
-    LinkedInProfile: "https://www.linkedin.com/in/kedarnath-chavan-768a92226",
-    GitHubProfile: "https://github.com/kedarnathpc",
-    InstagramProfile: "https://www.instagram.com/kedarnathpc",
+      "https://res.cloudinary.com/dk9fzvliu/image/upload/v1726411387/PurvaPatil_caaecv.png",
+    LinkedInProfile: "https://www.linkedin.com/in/purva-patil-a29175270/?trk=public-profile-join-page",
+    GitHubProfile: "https://github.com/piya200",
+    InstagramProfile: "https://www.instagram.com/purva_patil_2108?igsh=MXIzdzh1ZzVjZzVkbw==",
   },
+
   {
-    Name: "Rajan Lature",
+    Name: "Aditya Aparadh",
     Board: "Main",
     Position: "Club Service Director",
     Photo:
-      "https://res.cloudinary.com/du3cgjere/image/upload/v1693624856/sait%20images/Rajan_Lature_a9m5zm.jpg",
-    LinkedInProfile: "https://www.linkedin.com/in/rajan-lature-32aa9522a/",
-    GitHubProfile: "https://github.com/rajan-1700",
-    InstagramProfile: "https://www.instagram.com/rajan_lature/",
+      "https://res.cloudinary.com/du3cgjere/image/upload/v1693624824/sait%20images/Aditya_Aparadh_o603b1.jpg",
+    LinkedInProfile: "https://www.linkedin.com/in/aditya-aparadh/",
+    GitHubProfile: "https://github.com/AdityaAparadh",
+    InstagramProfile: "-",
   },
+
   {
-    Name: "Kalash Patil",
+    Name: "Sandesh Shinde",
     Board: "Main",
     Position: "Club Service Director",
     Photo:
-      "https://res.cloudinary.com/du3cgjere/image/upload/v1693624846/sait%20images/Kalash_Patil_vvl6jx.jpg",
-    LinkedInProfile: "https://www.linkedin.com/in/kalashpatil33/",
-    GitHubProfile: "https://github.com/kalashpatil33",
-    InstagramProfile: "https://www.instagram.com/kalash.patil33/",
+      "https://res.cloudinary.com/dptdrzycj/image/upload/v1704356554/20231231_175933_-_Sandesh_Shinde_bhcg8x.jpg",
+    LinkedInProfile: "https://www.linkedin.com/in/sandesh-shinde-s1730?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
+    GitHubProfile: "https://github.com/sandeshshinde30",
+    InstagramProfile: "https://www.instagram.com/sandesh_shinde_1730/",
   },
+
   {
-    Name: "Saurabh Salunke",
+    Name: "Tanmay Shingde",
     Board: "Main",
     Position: "Web Developer",
     Photo:
-      "https://res.cloudinary.com/du3cgjere/image/upload/v1704534653/sait%20images/cglrkt5rrlzsmvqvr8fu.jpg",
-    LinkedInProfile: "https://www.linkedin.com/in/saurabh-salunke-560448245/",
-    GitHubProfile: "https://github.com/SaurabhS55",
-    InstagramProfile: "https://www.instagram.com/saurabhsalunke555/",
+      "https://res.cloudinary.com/dk9fzvliu/image/upload/v1726410612/TanmayShingde_qnltb7.png",
+    LinkedInProfile: "https://www.linkedin.com/in/tanmayshingde?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
+    GitHubProfile: "https://github.com/tanmay-8",
+    InstagramProfile: "-",
   },
+
   {
-    Name: "Kaustubh Suroshi",
+    Name: "Hemant Sharma",
     Board: "Main",
     Position: "Web Developer",
     Photo:
-      "https://res.cloudinary.com/du3cgjere/image/upload/v1693624847/sait%20images/Kaustubh_Suroshi_r9skyc.jpg",
-    LinkedInProfile: "https://www.linkedin.com/in/kaustubh-suroshi-75434723a",
-    GitHubProfile: "https://github.com/kaustubh-555",
-    InstagramProfile: "kaustubh_5500",
+      "https://res.cloudinary.com/dk9fzvliu/image/upload/v1726409593/HemantSharma_rlybrr.jpg",
+    LinkedInProfile: "https://www.linkedin.com/in/hemant-sharma-690587256/",
+    GitHubProfile: "https://github.com/hemantsharma2004",
+    InstagramProfile: "-",
   },
+
   {
-    Name: "Shailendra Mahadule",
+    Name: "Satej Sawant",
     Board: "Main",
-    Position: "Public Relations Officer",
+    Position: "Programming Expert",
     Photo:
-      "https://res.cloudinary.com/du3cgjere/image/upload/v1693624861/sait%20images/Shailendra_Mahadule_lbwphl.jpg",
-    LinkedInProfile:
-      "https://www.linkedin.com/in/shailendra-mahadule-96955b200",
-    GitHubProfile: "https://github.com/Shailendra-08",
-    InstagramProfile: "https://www.instagram.com/ayushmahadule/",
-  },
-  
-  {
-    Name: "Siddhi Phatak",
-    Board: "Main",
-    Position: "Public Relations Officer",
-    Photo:
-      "https://res.cloudinary.com/du3cgjere/image/upload/v1693624878/sait%20images/Siddhi_Phatak_csekgr.jpg",
-    LinkedInProfile: "https://www.linkedin.com/in/siddhi-phatak-491603231",
-    GitHubProfile: "https://github.com/SIDDHI100",
+      "https://res.cloudinary.com/du3cgjere/image/upload/v1693624861/sait%20images/Satej_Sawant_p4jvnv.jpg",
+    LinkedInProfile: "https://www.linkedin.com/in/satej-sawant-92a8a4269/",
+    GitHubProfile: "https://github.com/SatejSawant45",
     InstagramProfile:
-      "https://instagram.com/siddhi__100?utm_source=qr&igshid=ZDc4ODBmNjlmNQ%3D%3D",
+      "https://www.instagram.com/satej.s?igsh=ZWVrc2Rua29td2p2",
   },
+
   {
-    Name: "Mohammadsaad Mulla",
+    Name: "Aditya Khandare",
+    Board: "Main",
+    Position: "Programming Expert",
+    Photo:
+      "https://res.cloudinary.com/dk9fzvliu/image/upload/v1726408725/AdityaKhandare_kcv2m9.jpg",
+    LinkedInProfile: "https://www.linkedin.com/in/aditya-khandare-4a3b91252/",
+    GitHubProfile: "https://github.com/Aditya-8320",
+    InstagramProfile:
+      "https://www.instagram.com/adityakhandare8320/",
+  },
+
+  {
+    Name: "Vidyadhar Dinde",
+    Board: "Main",
+    Position: "Programming Expert",
+    Photo:
+      "https://res.cloudinary.com/du3cgjere/image/upload/v1693624879/sait%20images/Vidyadhar_dhinde_maqujm.jpg",
+    LinkedInProfile: "https://www.linkedin.com/in/vidyadhar-dinde-447371252/",
+    GitHubProfile: "https://github.com/vidyadhardinde001",
+    InstagramProfile:
+      "https://www.instagram.com/_vidyadhar_d/",
+  },
+
+  {
+    Name: "Divya Tarkase",
     Board: "Main",
     Position: "Public Relation Officer",
     Photo:
-      "https://res.cloudinary.com/du3cgjere/image/upload/v1693624858/sait%20images/saad_k6kwaa.jpg",
-    LinkedInProfile: "https://www.linkedin.com/in/c0delust/",
-    GitHubProfile: "https://github.com/c0delust",
-    InstagramProfile: "https://www.instagram.com/c0delust/",
+      "https://res.cloudinary.com/du3cgjere/image/upload/v1693624845/sait%20images/Divya_Tarkase_zesjnq.jpg",
+    LinkedInProfile:
+      "https://www.linkedin.com/in/divya-tarkase-56ab30265?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
+    GitHubProfile: "https://github.com/DivyaTarkase",
+    InstagramProfile: "-",
   },
+
   {
-    Name: "Himanshu Mude",
+    Name: "Shreyas Kamble",
+    Board: "Main",
+    Position: "Public Relation Officer",
+    Photo:
+      "https://res.cloudinary.com/dk9fzvliu/image/upload/v1726410424/ShreyasKamble_ccumsp.jpg",
+    LinkedInProfile: "https://www.linkedin.com/in/shreyas-kamble-aa465922a",
+    GitHubProfile: "https://github.com/shreyaskamble1612",
+    InstagramProfile:
+      "https://www.instagram.com/shreyas__1612?igsh=ejhnZnd1cjBndXg3",
+  },
+
+  {
+    Name: "Shreyash Pawar",
+    Board: "Main",
+    Position: "Public Relation Officer",
+    Photo:
+        "https://res.cloudinary.com/dk9fzvliu/image/upload/v1726411837/ShreyashPawar_umkfyc.jpg",
+    LinkedInProfile: "https://www.linkedin.com/in/shreyash-pawar-386486249",
+    GitHubProfile: "https://github.com/Ssp63",
+    InstagramProfile: "https://www.instagram.com/s.s.p_63/?hl=en",
+  },
+
+  {
+    Name: "Anuja Suntnur",
     Board: "Main",
     Position: "Aptitude Developer",
     Photo:
-      "https://res.cloudinary.com/du3cgjere/image/upload/v1693624851/sait%20images/Himanshu_Mude_ccryzb.jpg",
-    LinkedInProfile: "https://www.linkedin.com/in/himanshu-mude-48668622a",
-    GitHubProfile: "github.com/HimanshuMude",
+      "https://res.cloudinary.com/du3cgjere/image/upload/v1693624844/sait%20images/Anuja_Suntnur_gdbyof.jpg",
+    LinkedInProfile: "https://www.linkedin.com/in/anuja-suntnur-93176225b?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
+    GitHubProfile: "https://github.com/anuja2004",
     InstagramProfile:
-      "https://instagram.com/himanshu_mude?utm_source=qr&igshid=MzNlNGNkZWQ4Mg%3D%3D",
+      "https://www.instagram.com/_anuja_suntnur/",
   },
+  
   {
-    Name: "Parth Kulkarni ",
+    Name: "Vedant Panari",
     Board: "Main",
     Position: "Aptitude Developer ",
     Photo:
-      "https://res.cloudinary.com/du3cgjere/image/upload/v1693624878/sait%20images/Parth_Kulkarni_nb4iqy.jpg",
-    LinkedInProfile: "https://www.linkedin.com/in/parth-kulkarni-554978233",
-    GitHubProfile: "https://github.com/Parthk26",
+      "https://res.cloudinary.com/du3cgjere/image/upload/v1693624878/sait%20images/Vedant_Panari_h6huyi.jpg",
+    LinkedInProfile: "https://www.linkedin.com/in/vedant-panari-379869287?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
+    GitHubProfile: "https://github.com/Vedant-Panari",
     InstagramProfile:
-      "https://instagram.com/parth_k026?utm_source=qr&igshid=MzNlNGNkZWQ4Mg%3D%3D",
+      "https://www.instagram.com/vedantpanari?igsh=MW5qbGl5YWp1OHhoZQ==",
   },
+
   {
-    Name: "Sana Mulani",
-    Board: "Main",
-    Position: "Art Designer & Editor ",
-    Photo:
-      "https://res.cloudinary.com/du3cgjere/image/upload/v1693624858/sait%20images/Sana_Mulani_zvqtpx.jpg",
-    LinkedInProfile: "https://www.linkedin.com/in/sana-mulani-758074258",
-    GitHubProfile: "https://github.com/SanaMulani1108",
-    InstagramProfile:
-      "https://instagram.com/sana_mulani1108?igshid=NGExMmI2YTkyZg==",
-  },
-  {
-    Name: "Prerna  Pawar ",
+    Name: "Tamnna Malgave",
     Board: "Main",
     Position: "Art designer and Editor ",
     Photo:
-      "https://res.cloudinary.com/du3cgjere/image/upload/v1693624868/sait%20images/Prerna_Pawar_tjnp53.jpg",
-    LinkedInProfile: "https://www.linkedin.com/in/prerna-pawar-673531234",
-    GitHubProfile: "https://github.com/Prerna-Pawar",
-    InstagramProfile: "https://www.instagram.com/prerna_pawar.14/saved/",
+      "https://res.cloudinary.com/dk9fzvliu/image/upload/v1726408959/TamannaMalgave_lcewex.jpg",
+    LinkedInProfile: "https://www.linkedin.com/in/tamnna-malgave-a23461299",
+    GitHubProfile: "https://github.com/TamnnaMalgave",
+    InstagramProfile: "-",
   },
+
   {
-    Name: "Shravani Umare",
+    Name: "Vidyadhar Dinde",
+    Board: "Main",
+    Position: "Art designer and Editor ",
+    Photo:
+      "https://res.cloudinary.com/du3cgjere/image/upload/v1693624879/sait%20images/Vidyadhar_dhinde_maqujm.jpg",
+    LinkedInProfile: "https://www.linkedin.com/in/vidyadhar-dinde-447371252/",
+    GitHubProfile: "https://github.com/vidyadhardinde001",
+    InstagramProfile: "https://www.instagram.com/_vidyadhar_d/",
+  },
+
+  {
+    Name: "Aryan Babar",
+    Board: "Main",
+    Position: "Art designer and Editor ",
+    Photo:
+      "https://res.cloudinary.com/dk9fzvliu/image/upload/v1726409894/AryanBabar_awvjig.jpg",
+    LinkedInProfile: "https://www.linkedin.com/in/aryan-babar-84771a259",
+    GitHubProfile: "https://github.com/Aaryalegend",
+    InstagramProfile: "https://www.instagram.com/aryanbabar5796",
+  },
+
+  {
+    Name: "Shreyash Pawar",
     Board: "Main",
     Position: "Art designer and Editor",
     Photo:
-      "https://res.cloudinary.com/du3cgjere/image/upload/v1693624874/sait%20images/Shravani_Umare_aygb68.jpg",
-    LinkedInProfile: "https://www.linkedin.com/in/shravaniumare03",
-    GitHubProfile: "shravani2003",
-    InstagramProfile: "shravni.umare3",
+        "https://res.cloudinary.com/dk9fzvliu/image/upload/v1726411837/ShreyashPawar_umkfyc.jpg",
+    LinkedInProfile: "https://www.linkedin.com/in/shreyash-pawar-386486249",
+    GitHubProfile: "https://github.com/Ssp63",
+    InstagramProfile: "https://www.instagram.com/s.s.p_63/?hl=en",
+  },
+
+  {
+    Name: "Gaurav Gokhale",
+    Board: "Main",
+    Position: "Art designer and Editor",
+    Photo:
+      "https://res.cloudinary.com/dk9fzvliu/image/upload/v1726409111/GauravGokhale_nwtdin.jpg",
+    LinkedInProfile: "https://www.linkedin.com/in/gauravgokhale07/",
+    GitHubProfile: "https://github.com/Grg07",
+    InstagramProfile: "-",
   },
 ];
 export default data;
